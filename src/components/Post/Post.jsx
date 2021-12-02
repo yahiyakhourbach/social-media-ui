1487c84aa95b7517605e698e36f7f255109966bd
yahiyakhourbach <yahyakharbach9@gmail.com>
2021-12-02 20:58:51 +0000

    commit dynamic likes

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -1,21 +1,22 @@
 import { MoreVert } from '@material-ui/icons';
 import './Post.css';
 import { users } from '../../DummyData';
+import { useState } from 'react';
 const Post = ({ post }) => {
-  const img = users.filter((user) => {
-    if (user.id === post.userid) {
-      return user.img;
-    } else {
-      return '';
-    }
-  });
+  const user = users.filter((user) => user.id === post.userid)[0];
+  const [like, setLike] = useState(post.like);
+  const [isliked, setIsliked] = useState(false);
+  const likeHandler = () => {
+    setLike(isliked ? like - 1 : like + 1);
+    setIsliked(!isliked);
+  };
   return (
     <div className="Post">
       <div className="postWrapper">
         <div className="posttop">
           <div className="post-top-left">
-            <img src={img[0].img} alt="" className="imgpost" />
-            <span className="username-post">{img[0].username}</span>
+            <img src={user.img} alt="" className="imgpost" />
+            <span className="username-post">{user.username}</span>
             <span className="postDate">{post.date}</span>
           </div>
           <div className="post-top-right">
@@ -28,9 +29,14 @@ const Post = ({ post }) => {
         </div>
         <div className="postbottom">
           <div className="postleftbottom">
+            <img
+              src="/assests/like.png"
+              alt=""
+              onClick={likeHandler}
+              className="emojiPost"
+            />
             <img src="/assests/heart.png" alt="" className="emojiPost" />
-            <img src="/assests/like.png" alt="" className="emojiPost" />
-            <span className="postlikecounter">{post.like} likes</span>
+            <span className="postlikecounter">{like} likes</span>
           </div>
           <div className="postrightbottom">
             <span className="postcommenttext">9 comments</span>
